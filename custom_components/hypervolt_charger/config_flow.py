1949cb64f32e1e083644e29177cd7a262201ad65
Graham Dean <graham@gndean.com>
2022-12-28 11:48:51 +0000

    Changed to a more user friendly title

--- custom_components/hypervolt_charger/config_flow.py
+++ custom_components/hypervolt_charger/config_flow.py
@@ -97,7 +97,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             # Store charger ID into config
             user_input["charger_id"] = info["charger_id"]
 
-            return self.async_create_entry(title=info["charger_id"], data=user_input)
+            return self.async_create_entry(
+                title=f"Hypervolt {info['charger_id']}", data=user_input
+            )
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
